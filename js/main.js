4a8e3de86f6378fe62c11933a02cb878cbb0be91
alireza <alireza.abedini78@gmail.com>
2021-07-08 20:37:07 +0430

    slide changer buttonn added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-var slideIndex = 0;
-showSlides();
-
 function showSlides() {
   var i;
   var slides = document.querySelectorAll(".slider__image")
@@ -15,3 +12,25 @@ function showSlides() {
 }
 
 
+function changeSlides(offset){
+    var i;
+    var slides = document.querySelectorAll(".slider__image")
+    console.log(slides.length)
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex += offset;
+    console.log(`offset is ${offset}`)
+    console.log(`slide index is ${slideIndex}`)
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex <= 0) {slideIndex = slides.length}
+    slides[slideIndex-1].style.display = "block";
+}
+
+
+var slideIndex = 0;
+showSlides();
+const next_btn = document.querySelector("#next")
+next_btn.addEventListener('click', function(){changeSlides(1)}, false)
+const prev_btn = document.querySelector("#prev")
+prev_btn.addEventListener('click', function(){changeSlides(-1)}, false)
