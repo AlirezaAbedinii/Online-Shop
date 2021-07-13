6b3d7ec46a2402040c3f656fb63b293673d89874
salva <salva_k4@yahoo.com>
2021-07-13 16:56:31 +0430

    sign-in-modal

--- js/signin.js
+++ js/signin.js
@@ -3,6 +3,7 @@ const email= document.getElementById('email');
 const pass= document.getElementById('pass');
 const button=document.getElementById('button');
 console.log(email);
+const user_pass_list=[{mail:"salva_k4@yahoo.com",password:"12345678a"},{mail:"a@b.c",password:"12345678b"},{email:"a@b.d",password:"12345678c"}];
 
 email.addEventListener('keyup',(event)=>{
     //event.preventDefault();
@@ -14,6 +15,7 @@ pass.addEventListener('keyup',(event)=>{
 });
 
 const checkMail = () => {
+    let check=false;
     const emailValue = email.value.trim(); //removing whitespace from end and begining
     if (emailValue === ''){ //error if input is deleted(empty)
         setError(email,'ایمیل نمی‌تواند خالی باشد');
@@ -24,11 +26,13 @@ const checkMail = () => {
         setError(email,'ایمیل باید کمتر از ۲۵۵ کاراکتر باشد');
     } else{
         setSuccess(email);
+        check=true;
     }
-
+    return check;
 
 }
 const checkPass=() => {
+    let check=false;
     const passValue=pass.value;
     if(passValue===''){
         setError(pass,"رمز عبور نمی‌تواند خالی باشد");
@@ -43,7 +47,9 @@ const checkPass=() => {
         setError(pass,'رمز عبور باید شامل حروف نیز باشد');
     }else{
         setSuccess(pass);
+        check=true;
     }
+    return check;
 }
 
 const setError = (input, msg) => {
@@ -71,5 +77,44 @@ function ValidateEmail(mail)
     //alert("You have entered an invalid email address!")
     return (false);
 }
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+  setMessage();
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function setMessage(){
+    is_valid_mail=checkMail();
+    is_valid_pass=checkPass();
+    passValue=pass.value;
+    mailValue=email.value.trim();
+    modal_msg=document.getElementById('modal__msg');
+    if(is_valid_mail==false || is_valid_pass==false){
+        modal_msg.innerHTML='فیلدهای مشخص شده را پر کنید';
+    }
+    else{
+        modal_msg.innerHTML='hi'; 
+    }
+    
+}
 
