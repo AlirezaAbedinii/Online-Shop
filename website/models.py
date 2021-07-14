3465be05453c5beb0733e30cbeee1607180fd14a
alireza <alireza.abedini78@gmail.com>
2021-07-14 14:41:30 +0430

    some db modules added

--- website/models.py
+++ website/models.py
@@ -1,3 +1,26 @@
 from . import db
 from flask_login import UserMixin
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.string(100), primary_key=True)
+    password = db.Column(db.string(100))
+    first_name = db.Column(db.string(100))
+    last_name = db.Column(db.string(100))
+    address = db.Column(db.string(100))
+    charge = db.Column(db.string(100))
+    
+class Admin(db.Model):
+    id = db.Column(db.string(100), primary_key=True)
+    password = db.Column(db.string(100))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.string(100))
+    category = db.Column(db.string(100))
+    price = db.Column(db.Integer)
+    availability_number = db.Column(db.Integer)
+    sold_number = db.Column(db.Integer)
+    image = db.Column(db.string(100))
+    
+    
+    
