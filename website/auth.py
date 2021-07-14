edf958c921244c907751332c02844365470670f6
alireza <alireza.abedini78@gmail.com>
2021-07-14 11:07:32 +0430

    signup restructured

--- website/auth.py
+++ website/auth.py
@@ -5,3 +5,7 @@ auth = Blueprint('auth', __name__)
 @auth.route('/signin')
 def signin():
     return render_template("signin.html")
+
+@auth.route('/signup')
+def signup():
+    return render_template("signup.html")
