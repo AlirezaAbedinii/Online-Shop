c2a6247fbbf4705ef6d4a0d871208a102b340a0e
alireza <alireza.abedini78@gmail.com>
2021-07-14 13:18:58 +0430

    initialize db

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/signin')
+@auth.route('/signin', methods=['GET', 'POST'])
 def signin():
+    
     return render_template("signin.html")
 
-@auth.route('/signup')
+@auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
     return render_template("signup.html")
