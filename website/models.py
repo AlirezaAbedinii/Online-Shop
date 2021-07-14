6a675d6653288afccd58673812590a38aafb5ae8
alireza <alireza.abedini78@gmail.com>
2021-07-14 18:51:42 +0430

    Database Created V1

--- website/models.py
+++ website/models.py
@@ -1,26 +1,39 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.string(100), primary_key=True)
-    password = db.Column(db.string(100))
-    first_name = db.Column(db.string(100))
-    last_name = db.Column(db.string(100))
-    address = db.Column(db.string(100))
-    charge = db.Column(db.string(100))
+    id = db.Column(db.String(100), primary_key=True)
+    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    address = db.Column(db.String(100))
+    charge = db.Column(db.String(100))
     
 class Admin(db.Model):
-    id = db.Column(db.string(100), primary_key=True)
-    password = db.Column(db.string(100))
+    id = db.Column(db.String(100), primary_key=True)
+    password = db.Column(db.String(100))
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.string(100))
-    category = db.Column(db.string(100))
+    name = db.Column(db.String(100))
+    category = db.Column(db.String(100))
     price = db.Column(db.Integer)
     availability_number = db.Column(db.Integer)
     sold_number = db.Column(db.Integer)
-    image = db.Column(db.string(100))
+    image = db.Column(db.String(100))
     
+    
+class Receipt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100))
+    purchase_number = db.Column(db.Integer)
+    customer_first_name = db.Column(db.String(100))
+    customer_last_name = db.Column(db.String(100))
+    customer_address = db.Column(db.String(100))
+    total_price = db.Column(db.Integer)
+    date = db.Column(db.DateTime(timezone=True), default = func.now())
+    tracking_code = db.Column(db.String(100))
+    state = db.Column(db.String(100))
     
     
