b0b5d13f8801f8817b7e4ec25f81bf5781f0a1c6
salva <salva_k4@yahoo.com>
2021-07-17 00:41:54 +0430

    sign in and sign up with redirects

--- website/static/js/user.js
+++ website/static/js/user.js
@@ -50,109 +50,109 @@ const pass= document.getElementById("pass");
 const address=document.getElementById("address");
 
 
-//check pass content when user is typing
-pass.addEventListener('keyup',(event)=>{
-    //event.preventDefault();
-    checkPass();
-});
-//check first name content when user is typing
-fname.addEventListener('keyup',(event)=>{
-    //event.preventDefault();
-    checkName();
-});
-//check last name content when user is typing
-lname.addEventListener('keyup',(event)=>{
-    //event.preventDefault();
-    checkLName();
-});
-//check address content when user is typing
-address.addEventListener('keyup',(event)=>{
-    //event.preventDefault();
-    checkAddress();
-});
+// //check pass content when user is typing
+// pass.addEventListener('keyup',(event)=>{
+//     //event.preventDefault();
+//     checkPass();
+// });
+// //check first name content when user is typing
+// fname.addEventListener('keyup',(event)=>{
+//     //event.preventDefault();
+//     checkName();
+// });
+// //check last name content when user is typing
+// lname.addEventListener('keyup',(event)=>{
+//     //event.preventDefault();
+//     checkLName();
+// });
+// //check address content when user is typing
+// address.addEventListener('keyup',(event)=>{
+//     //event.preventDefault();
+//     checkAddress();
+// });
 
-//checks pass  box content
-const checkPass=() => {
-    //boolean, specifies the correctness
-    let check=false;
-    //box contnet value
-    const passValue=pass.value;
-    //check for empty
-    if(passValue===''){
-        setError(pass,"رمز عبور نمی‌تواند خالی باشد");
-    //check for min length
-    }else if(passValue.length<8){
-        setError(pass,"رمز عبور باید حداقل ۸ کاراکتر باشد");
-    }
-    //check foprm max length
-    else if(passValue.length>255){
-        setError(pass,"رمز باید کمتر از ۲۵۵ کاراکتر باشد");
-    }
-    //chek if  itcontains  numbers
-    else if(passValue.search(/\d/) == -1){
-        setError(pass,'رمز عبور باید شامل اعداد نیز باشد');
-    }
-    //checkif  it  contains charachters
-    else if(passValue.search(/[a-zA-Z]/) == -1){
-        setError(pass,'رمز عبور باید شامل حروف نیز باشد');
-    }else{
-        setSuccess(pass);
-        check=true;
-    }
-    return check;
-}
+// //checks pass  box content
+// const checkPass=() => {
+//     //boolean, specifies the correctness
+//     let check=false;
+//     //box contnet value
+//     const passValue=pass.value;
+//     //check for empty
+//     if(passValue===''){
+//         setError(pass,"رمز عبور نمی‌تواند خالی باشد");
+//     //check for min length
+//     }else if(passValue.length<8){
+//         setError(pass,"رمز عبور باید حداقل ۸ کاراکتر باشد");
+//     }
+//     //check foprm max length
+//     else if(passValue.length>255){
+//         setError(pass,"رمز باید کمتر از ۲۵۵ کاراکتر باشد");
+//     }
+//     //chek if  itcontains  numbers
+//     else if(passValue.search(/\d/) == -1){
+//         setError(pass,'رمز عبور باید شامل اعداد نیز باشد');
+//     }
+//     //checkif  it  contains charachters
+//     else if(passValue.search(/[a-zA-Z]/) == -1){
+//         setError(pass,'رمز عبور باید شامل حروف نیز باشد');
+//     }else{
+//         setSuccess(pass);
+//         check=true;
+//     }
+//     return check;
+// }
 
-//checks  the  name box content
-const checkName = () => {
-    //boolean, defines rhe  correctnes at  th end
-    let check=false;
-    const nameValue = fname.value.trim(); //removing whitespace from end and begining
-    if (nameValue === ''){ //error if input is deleted(empty)
-        setErrorName(fname,'نام نمی‌تواند خالی باشد');
-    }
-    else if(nameValue.length >255){//max 255 charachters
-        setErrorName(fname,'نام باید کمتر از ۲۵۵ کاراکتر باشد');
-    } else{
-        setSuccessName(fname);
-        check=true;
-    }
-    return check;
-}
+// //checks  the  name box content
+// const checkName = () => {
+//     //boolean, defines rhe  correctnes at  th end
+//     let check=false;
+//     const nameValue = fname.value.trim(); //removing whitespace from end and begining
+//     if (nameValue === ''){ //error if input is deleted(empty)
+//         setErrorName(fname,'نام نمی‌تواند خالی باشد');
+//     }
+//     else if(nameValue.length >255){//max 255 charachters
+//         setErrorName(fname,'نام باید کمتر از ۲۵۵ کاراکتر باشد');
+//     } else{
+//         setSuccessName(fname);
+//         check=true;
+//     }
+//     return check;
+// }
 
-//checks lastname box content
-const checkLName = () => {
-    //boolean, defnes the coorecteness at the end
-    let check=false;
-    const lnameValue = lname.value.trim(); //removing whitespace from end and begining
-    if (lnameValue === ''){ //error if input is deleted(empty)
-        setErrorLName(lname,'نام خوانوادگی نمی‌تواند خالی باشد');
-    }
-    else if(lnameValue.length >255){//max 255 charachters
-        setErrorLName(lname,'نام خوانوادگی باید کمتر از ۲۵۵ کاراکتر باشد');
-    } else{
-        setSuccessLName(lname);
-        check=true;
-    }
-    return check;
-}
+// //checks lastname box content
+// const checkLName = () => {
+//     //boolean, defnes the coorecteness at the end
+//     let check=false;
+//     const lnameValue = lname.value.trim(); //removing whitespace from end and begining
+//     if (lnameValue === ''){ //error if input is deleted(empty)
+//         setErrorLName(lname,'نام خوانوادگی نمی‌تواند خالی باشد');
+//     }
+//     else if(lnameValue.length >255){//max 255 charachters
+//         setErrorLName(lname,'نام خوانوادگی باید کمتر از ۲۵۵ کاراکتر باشد');
+//     } else{
+//         setSuccessLName(lname);
+//         check=true;
+//     }
+//     return check;
+// }
 
-//checks address bo  content
-const checkAddress = () => {
-    //boolean, defines correctness at the end
-    let check=false;
-    //address bax value without beginning or ending spaces
-    const addressValue = address.value; 
-    if (addressValue === ''){ //error if input is deleted(empty)
-        setError(address,'آدرس نمی‌تواند خالی باشد');
-    }
-    else if(addressValue.length >1000){//max 1000 charachters
-        setError(address,'آدرس باید کمتر از ۱۰۰۰ کاراکتر باشد');
-    } else{
-        setSuccess(address);
-        check=true;
-    }
-    return check;
-}
+// //checks address bo  content
+// const checkAddress = () => {
+//     //boolean, defines correctness at the end
+//     let check=false;
+//     //address bax value without beginning or ending spaces
+//     const addressValue = address.value; 
+//     if (addressValue === ''){ //error if input is deleted(empty)
+//         setError(address,'آدرس نمی‌تواند خالی باشد');
+//     }
+//     else if(addressValue.length >1000){//max 1000 charachters
+//         setError(address,'آدرس باید کمتر از ۱۰۰۰ کاراکتر باشد');
+//     } else{
+//         setSuccess(address);
+//         check=true;
+//     }
+//     return check;
+// }
 
 //change state to erroneous=>red border and small message
 const setError = (input, msg) => {
@@ -230,52 +230,97 @@ window.onclick = function(event) {
   }
 }
 //vhanging the message displayed in modal
-function setMessage(){
-    //correctness of boxes
-    is_valid_pass=checkPass();
-    is_valid_name=checkName();
-    is_valid_lname=checkLName();
-    is_valid_address=checkAddress();
+// function setMessage(){
+//     //correctness of boxes
+//     is_valid_pass=checkPass();
+//     is_valid_name=checkName();
+//     is_valid_lname=checkLName();
+//     is_valid_address=checkAddress();
 
-    //finding number of falses
-    const validation_arr=[is_valid_pass,is_valid_name,is_valid_lname,is_valid_address];
-    let false_num=0;
-    for(let i=0;i<validation_arr.length;i++){
-        if(validation_arr[i]===false){
-            false_num++;
-        }
-    }
+//     //finding number of falses
+//     const validation_arr=[is_valid_pass,is_valid_name,is_valid_lname,is_valid_address];
+//     let false_num=0;
+//     for(let i=0;i<validation_arr.length;i++){
+//         if(validation_arr[i]===false){
+//             false_num++;
+//         }
+//     }
 
+//     //value of the boxes
+//     passValue=pass.value;
+//     nameValue=fname.value.trim();
+//     lnameValue=lname.value.trim();
+//     addressValue=address.value;
+
+//     modal_msg=document.getElementById('modal__msg');
+//     //more than two boxes or wrong
+//     if(false_num>1){
+//         modal_msg.innerHTML='فیلدهای مشخص شده را کامل کنید';
+//     }
+//     //if only pass is invalid
+//     else if(is_valid_pass==false){
+//         modal_msg.innerHTML='فیلد رمز عبور را کامل کنید'; 
+//     }
+//     //if only name is invalid
+//     else if(is_valid_name==false){
+//          modal_msg.innerHTML='فیلد نام را کامل کنید'; 
+//     }
+//     //if only pass is invalid
+//     else if(is_valid_lname==false){
+//         modal_msg.innerHTML='فیلد نام خوانوادگی را کامل کنید'; 
+//     }
+//     //if only pass is invalid
+//     else if(is_valid_address==false){
+//         modal_msg.innerHTML='فیلد آدرس را کامل کنید'; 
+//     }
+//     //username and password are correct and defined
+//     else{
+//         modal_msg.innerHTML='ویرایش با موفقیت انجام شد';
+//     }
+    
+// }
+pass.addEventListener('keyup',(event)=>{
     //value of the boxes
     passValue=pass.value;
     nameValue=fname.value.trim();
     lnameValue=lname.value.trim();
     addressValue=address.value;
+    fetch(`${window.origin}/signup`, {
+        method: "POST",
+        body: JSON.stringify({
+            "first_name": nameValue,
+            "last_name": lnameValue,
+            "password": passValue,
+            "address": addressValue}),
+        headers: new Headers({"content-type": "application/json"}),
+        cache: 'no-cache'
+    })
+    .then(function (response){
+        if(response.status !== 200){
+            console.log(`bad request: ${response.status}`);
+            return;
+        }
+        response.json().then(function (data){
+            console.log(data);
+                if(data['pass']=="pass empty"){
+                    setError(pass,"رمز عبور نمی‌تواند خالی باشد");
+                }
+                else if(data['pass']=="pass min len invalid"){
+                    setError(pass,'رمز عبور باید حداقل ۸ کاراکتر باشد');
+                }
+                else if(data['pass']=="pass num invalid"){
+                    setError(pass,'رمز عبور باید شامل اعداد نیز باشد');
+                }
+                else if(data['pass']=="pass max len invalid"){
+                    setError(pass,'رمز باید کمتر از ۲۵۵ کاراکتر باشد');
+                }
+                else if(data['pass']=="pass char invalid"){
+                    setError(pass,'رمز عبور باید شامل حروف نیز باشد');
+                }
+                else{
+                    setSuccess(pass);
+                }
+            })
+    });
+});
 
-    modal_msg=document.getElementById('modal__msg');
-    //more than two boxes or wrong
-    if(false_num>1){
-        modal_msg.innerHTML='فیلدهای مشخص شده را کامل کنید';
-    }
-    //if only pass is invalid
-    else if(is_valid_pass==false){
-        modal_msg.innerHTML='فیلد رمز عبور را کامل کنید'; 
-    }
-    //if only name is invalid
-    else if(is_valid_name==false){
-         modal_msg.innerHTML='فیلد نام را کامل کنید'; 
-    }
-    //if only pass is invalid
-    else if(is_valid_lname==false){
-        modal_msg.innerHTML='فیلد نام خوانوادگی را کامل کنید'; 
-    }
-    //if only pass is invalid
-    else if(is_valid_address==false){
-        modal_msg.innerHTML='فیلد آدرس را کامل کنید'; 
-    }
-    //username and password are correct and defined
-    else{
-        modal_msg.innerHTML='ویرایش با موفقیت انجام شد';
-    }
-    
-}
