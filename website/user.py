7bdf045a86089da4244a2ddf50c4847d4d8ca0d8
alireza <alireza.abedini78@gmail.com>
2021-07-18 17:32:19 +0430

    some availability changes

--- website/user.py
+++ website/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy.sql.expression import and_
 from website.models import Admin, Basket, Category, Product, Receipt
 from flask import Blueprint, render_template, request, make_response, jsonify
 from . import db
@@ -13,8 +14,8 @@ user = Blueprint('user', __name__)
 def user_Tes():
     print('req received', file=sys.stdout)
     req = request.get_json()
-    user_id = current_user.id
-    # user_id = "alireza@arezz.com"
+    # user_id = current_user.id
+    user_id = "alireza@arezz.com"
     product_name = req['product_name']
     product_count = req['product_count']
     
@@ -24,7 +25,13 @@ def user_Tes():
         shopped_product = Product.query.filter_by(name = product_name).first()
         shopped_product.availability_number -= product_count
         
-        new_basket = Basket(customer_id = user_id, product_name=product_name, product_count=product_count)
+        if not Basket.query.filter(and_(Basket.customer_id == user_id, Basket.product_name == product_name)).first():
+            new_basket = Basket(customer_id = user_id, product_name=product_name, product_count=product_count)
+        else:
+             new_basket = Basket.query.filter(and_(Basket.customer_id == user_id, Basket.product_name == product_name)).first()
+             new_basket.product_count += product_count
+        
+        
         
         db.session.add(new_basket)
         db.session.commit()
